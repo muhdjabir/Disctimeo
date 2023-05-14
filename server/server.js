968f3762e3ce5771917ce8c9501a35bc11b0549b
muhdjabir <e0725438@u.nus.edu>
2023-05-14 23:02:32 +0800

    Added home route

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const authRoutes = require('./routes/auth');
 const clubRoutes = require('./routes/club');
 const scrimRoutes = require('./routes/scrim');
 const trialRoutes = require('./routes/trials');
+const homeRoutes = require('./routes/home');
 
 // express app
 const app = express();
@@ -20,12 +21,13 @@ app.use((req, res, next) => {
 });
 
 // Routes
+app.use('/', homeRoutes);
 app.use('/api/users', userRoutes); // Basically allows the use of routes from userRoutes
 app.use('/api/auth', authRoutes);
 app.use('/api/clubs', clubRoutes);
 app.use('/api/scrims', scrimRoutes);
 app.use('/api/trials', trialRoutes);
-
+ 
 //cConnect to DB
 mongoose.connect(process.env.MONG_URI)
     .then(() => {
